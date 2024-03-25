a8485c9d8b6be1a7083829a15fc13bfe83b64142
Thorbjørn Melheim <thomelheim@gmail.com>
2024-03-25 18:32:11 +0100

    Implemented ViewModel

---
+++ TodoList/TodoListDataContext.cs
@@ -0,0 +1,39 @@
+﻿using DatabindingSampleWPF;
+using System.Collections.ObjectModel;
+
+
+namespace TodoList
+{
+    public class TodoListDataContext : ObservableObject
+    {
+        public ObservableCollection<Todo> Todos { get;  } = new ObservableCollection<Todo>();
+
+
+        public void AddTodo(Todo todo)
+        {
+            if (todo == null) return;
+            Todos.Add(todo);
+        }
+
+        private Todo? _selectedTodo;
+
+        public Todo? SelectedTodo
+        {
+            get => _selectedTodo;
+            set
+            {
+                Set(ref _selectedTodo, value);
+                RaisePropertyChanged(nameof(IsRemoveTodoEnabled));
+            }
+        }
+
+        public bool IsRemoveTodoEnabled => SelectedTodo != null;
+
+        public void RemoveTodo()
+        {
+            if(SelectedTodo == null) return;
+            Todos.Remove(SelectedTodo);
+        }
+    }
+
+}
