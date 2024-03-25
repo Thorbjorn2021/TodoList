4af864c84399a5e94ad4df4edd2497be6180c762
Thorbjørn Melheim <thomelheim@gmail.com>
2024-03-25 13:19:34 +0100

    Model implemented

---
+++ TodoList/Todo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList
+{
+    public class Todo
+    {
+
+        public Todo(string title, string description, string priority) {
+            Title = title;
+            Description = description;
+            Priority = priority;
+        }
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string Priority { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{Title} | {Description} | {Priority}";
+        }
+    }
+}
