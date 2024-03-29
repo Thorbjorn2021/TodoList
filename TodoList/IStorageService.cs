e5f95e0203b0fb6516d7766a4bc8ebd579eb35d3
Thorbjørn Melheim <thomelheim@gmail.com>
2024-03-29 13:21:01 +0100

    StorageService interface and class that implements it

---
+++ TodoList/IStorageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList
+{
+    public interface IStorageService
+    {
+        public void StoreData(List<Todo> data);
+        public List<Todo> RetrieveData();
+    }
+}
---
+++ TodoList/JsonFileStorageService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TodoList
+{
+    public class JsonFileStorageService : IStorageService
+    {
+        private string _filePath;
+
+        public JsonFileStorageService(string filePath)
+        {
+            _filePath = filePath;
+        }
+        public List<Todo> RetrieveData()
+        {
+            if(File.Exists(_filePath))
+            {
+                string json = File.ReadAllText(_filePath);
+                return JsonSerializer.Deserialize<List<Todo>>(json);
+            }
+            else
+            {
+                return new List<Todo>();
+            }
+            
+            
+
+            
+        }
+
+        public void StoreData(List<Todo> data)
+        {
+            string json = JsonSerializer.Serialize(data);
+            File.WriteAllText(_filePath, json);
+        }
+    }
+}
